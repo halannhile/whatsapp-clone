20a239b7bd79f278a8d6d5d16ff84bbf8c27806a
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-03-07 17:31:46 +1100

    added some icons and avatar for top panel of sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import './Sidebar.css';
+import ChatIcon from '@mui/icons-material/Chat';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Avatar, IconButton } from '@mui/material';
+// import myImage from './assets/my-image.png';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-        <h1>I am a sidebar</h1>
+        <div className="sidebar__header">
+          <div className="sidebar__headerRight">
+          
+            <Avatar src="./assets/my-image.png"/>
+
+            <IconButton> 
+              <DonutLargeIcon/>
+            </IconButton>
+            
+            <IconButton> 
+              <ChatIcon/>
+            </IconButton>
+            
+            <IconButton> 
+              <MoreVertIcon/>
+            </IconButton>
+
+          </div>
+        </div>
     </div>
   )
 }
