7e5f3e453c82d5b2b14575356c4414b818c32543
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-03-07 17:03:18 +1100

    created sidebar and chat components, did css styling

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCp1bkvsx-YVH_GCYHvY2v669-UuHPY98Y",
+    authDomain: "whatsapp-mern-858a6.firebaseapp.com",
+    projectId: "whatsapp-mern-858a6",
+    storageBucket: "whatsapp-mern-858a6.appspot.com",
+    messagingSenderId: "661806949310",
+    appId: "1:661806949310:web:e51f515a584e7afdf98fef"
+  };
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// webpack.config.js
+
+module.exports = {
+    // ...
+    devServer: {
+      setupMiddlewares: function(devServer) {
+        // your middleware setup code here
+      },
+      // other dev server options
+    },
+    // ...
+  };
+  
