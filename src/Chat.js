a4dc664e7208005d33f0f56c0317902bd85c68d3
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-03-08 23:09:48 +1100

    finished styling chat portion's header

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,34 @@
+import { SearchOutlined, AttachFile, MoreVert } from '@mui/icons-material'
+import { Avatar, IconButton } from '@mui/material'
 import React from 'react'
 import "./Chat.css"
 
 function Chat() {
   return (
     <div className="chat">
-        <h1>This is the chat</h1>
+
+        <div className="chat__header">
+          
+          <Avatar/>
+
+          <div className="chat__headerInfo">
+            <h3>Room name</h3>
+            <p>last seen at...</p>
+          </div>
+
+          <div className="chat_headerRight">
+            <IconButton><SearchOutlined/></IconButton>
+            <IconButton><AttachFile/></IconButton>
+            <IconButton><MoreVert/></IconButton>
+          </div>
+          
+          
+        </div>
+
+        <div className="chat__body">
+          <p>This is a message</p>
+        </div>
+
     </div>
   )
 }
