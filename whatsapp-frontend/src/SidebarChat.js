d3084dfc364076e09b87668a2da332c921f83a7d
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-03-14 21:29:48 +1100

    fixed room name error

--- whatsapp-frontend/src/SidebarChat.js
+++ whatsapp-frontend/src/SidebarChat.js
@@ -2,17 +2,17 @@ import { Avatar } from '@mui/material';
 import React from 'react';
 import "./SidebarChat.css";
 
-function SidebarChat() {
+function SidebarChat(props) {
   return (
     <div className="sidebarChat">
         
         <Avatar/>
         <div className="sidebarChat__info">
-            <h2>Room name</h2>
+            <h2>{props.roomName}</h2>
             <p>This is the last message</p>
         </div>
     
-    </div>
+    </div>  
   )
 }
 
