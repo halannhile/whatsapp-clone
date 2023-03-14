0fd30a1676a675920025cc82057a5de1fb7f985d
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-03-14 21:53:20 +1100

    changed timestamp from String to Number in dbMessages.js

--- whatsapp-backend/dbMessages.js
+++ whatsapp-backend/dbMessages.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const whatsappSchema = mongoose.Schema({
     message: String,
     name: String,
-    timestamp: String,
+    timestamp: Number,
     received: Boolean
 });
 
