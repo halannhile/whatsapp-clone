2e26f1f0b302087a910eba672d2eb995ca3b662c
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-03-14 21:48:07 +1100

    changed axios url back to localhost

--- whatsapp-frontend/src/axios.js
+++ whatsapp-frontend/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: 'http://localhost:9000',
-    baseURL: 'https://nhile-whatsapp-mern.netlify.app/',
+    baseURL: 'http://localhost:9000',
+    // baseURL: 'https://nhile-whatsapp-mern.netlify.app/',
 });
 
 export default instance;
