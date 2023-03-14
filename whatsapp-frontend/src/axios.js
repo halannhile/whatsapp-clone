206646a36f5af0a265fe2522816f5173833b1c82
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-03-14 21:46:08 +1100

    changed axios.js to deployed app's url

--- whatsapp-frontend/src/axios.js
+++ whatsapp-frontend/src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9000',
+    // baseURL: 'http://localhost:9000',
+    baseURL: 'https://nhile-whatsapp-mern.netlify.app/',
 });
 
 export default instance;
